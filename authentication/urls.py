fda73349be9bd31f35e93f738b754ac624008d4b
no name <mail.nice@protonmail.com>
2022-01-18 20:08:56 +0100

    add option to create user trough create_user.html

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('create_user/', views.CreateUserView.as_view(), name="CreateUser"),
     path('', include('django.contrib.auth.urls')),
-    # path('accounts/create_user', )
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import FormView
+
+from . import forms
+from . import models
+
 
 # Create your views here.
+
+class CreateUserView(FormView):
+    template_name = 'registration/create_user.html'
+    form_class = forms.CreateUserForm
+    success_url = '/accounts/login/'
+
+    def form_valid(self, form):
+        username = self.request.POST["username"]
+        password = self.request.POST["password"]
+
+        user = models.User.objects.create_user(username=username, password=password)
+        user.save()
+
+        return HttpResponseRedirect(reverse('login'))
