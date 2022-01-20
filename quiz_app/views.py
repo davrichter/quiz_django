ff4c30d9e50c3e80419d82af8af899f60d1867ca
no name <mail.nice@protonmail.com>
2022-01-20 15:43:51 +0100

    comment out unused imports

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,14 +1,14 @@
 """
     views for quiz_app
 """
-
+"""
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.edit import FormView
 
 from . import forms
 from . import models
-
+"""
 
 # Create your views here.
 
