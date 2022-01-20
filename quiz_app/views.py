07651a1fb6c00ca57495aca3dd5e76c9e4cbab66
no name <mail.nice@protonmail.com>
2022-01-20 15:43:08 +0100

    remove copy and pasted view, added module-docstring to views.py

--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,3 +1,7 @@
+"""
+    views for quiz_app
+"""
+
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.edit import FormView
@@ -8,10 +12,3 @@ from . import models
 
 # Create your views here.
 
-class CreateQuizView(FormView):
-    template_name = 'registration/create_user.html'
-    form_class = forms.CreateQuizForm
-    success_url = '/accounts/login/'
-
-    def form_valid(self, form):
-        return HttpResponseRedirect(reverse('login'))
