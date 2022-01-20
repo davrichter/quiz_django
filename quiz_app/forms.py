d0ceea961be5fed16e518efd01d4e98751190cf5
no name <mail.nice@protonmail.com>
2022-01-20 15:41:57 +0100

    add module-docstring to forms.py

--- quiz_app/forms.py
+++ quiz_app/forms.py
@@ -1,8 +1,13 @@
+"""
+    forms for the quit_app
+"""
+
 from django import forms
 
 from authentication.models import User
 
 
 class CreateQuizForm(forms.Form):
+    """form for validating the input while when the user creates a quiz"""
     title = forms.CharField(max_length=100)
     user = forms.ModelChoiceField(queryset=User)
