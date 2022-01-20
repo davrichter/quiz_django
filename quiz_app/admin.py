a25a47adf47ce6fe82cdb9604495a30322136356
no name <mail.nice@protonmail.com>
2022-01-20 15:27:54 +0100

    add quiz_app models to admin site

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Quiz)
+admin.site.register(models.Question)
+admin.site.register(models.Option)
+admin.site.register(models.Comment)
