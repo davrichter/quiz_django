700b90d487e0760f6814ebcd22458cbe62140131
no name <mail.nice@protonmail.com>
2022-01-20 14:02:30 +0100

    add docstrings to models.py in quiz_app

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -6,6 +6,7 @@ from authentication.models import User
 # Create your models here.
 
 class Quiz(models.Model):
+    """a quiz which users can create, like, dislike or add comments"""
     title = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.PositiveIntegerField(name="Likes")
@@ -17,16 +18,23 @@ class Quiz(models.Model):
 
 
 class Question(models.Model):
+    """
+    a single question in a quiz, a quit can have multiple questions
+    optionally there can be a time limit for each question
+    """
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    time = models.PositiveIntegerField(verbose_name="Time to solve the question", name="Time")
+    time = models.PositiveIntegerField(verbose_name="Time to solve the question", name="Time", null=True)
 
 
 class Option(models.Model):
+    """an option which belongs to a question, a question can have multiple options"""
     text = models.CharField(max_length=200)
     Question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
+    """a comment which users can write and which always belongs to a quiz"""
     text = models.CharField(max_length=1000)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
