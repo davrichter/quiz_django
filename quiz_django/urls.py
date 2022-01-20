dfa3c4908668f594d3add8a0e2dc7f86ff0ec2d4
no name <mail.nice@protonmail.com>
2022-01-20 15:35:17 +0100

    add module-docstring to urls.py

--- quiz_django/urls.py
+++ quiz_django/urls.py
@@ -1,3 +1,9 @@
+""" quiz_django URL Configuration
+
+    The `urlpatterns` list routes URLs to views. For more information please see:
+        https://docs.djangoproject.com/en/4.0/topics/http/urls/
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 
