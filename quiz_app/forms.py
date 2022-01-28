396f8df77534cb608ca5e86f8908c221039267e0
no name <mail.nice@protonmail.com>
2022-01-28 12:05:16 +0100

    Remove unused import.

--- quiz_app/forms.py
+++ quiz_app/forms.py
@@ -4,8 +4,6 @@
 
 from django import forms
 
-from authentication.models import User
-
 
 class CreateQuizForm(forms.Form):
     """form for validating the input while when the user creates a quiz"""
