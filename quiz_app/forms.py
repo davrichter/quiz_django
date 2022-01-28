48c9ab6475ab3aa008d5b0f41e951ab526c48788
no name <mail.nice@protonmail.com>
2022-01-28 12:04:42 +0100

    Add question create form.

--- quiz_app/forms.py
+++ quiz_app/forms.py
@@ -11,3 +11,12 @@ class CreateQuizForm(forms.Form):
     """form for validating the input while when the user creates a quiz"""
     title = forms.CharField(max_length=100)
     thumbnail = forms.ImageField()
+
+
+class CreateQuestionForm(forms.Form):
+    title = forms.CharField(max_length=200)
+    time = forms.IntegerField(
+        verbose_name="Time to solve the question",
+        name="Time",
+        null=True
+    )
